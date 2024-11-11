1334d73a6652d7b747c535c186c11a5a14dd23ec
redCat <yana.sitkowecz@yandex.ru>
2024-11-11 12:18:26 +0300

    Add function for output

--- src/main/java/org/acme/projectjobschedule/app/ProjectJobScheduleApp.java
+++ src/main/java/org/acme/projectjobschedule/app/ProjectJobScheduleApp.java
@@ -8,7 +8,7 @@ import org.acme.projectjobschedule.domain.Job;
 import org.acme.projectjobschedule.domain.Project;
 import org.acme.projectjobschedule.domain.ProjectJobSchedule;
 import org.acme.projectjobschedule.solver.ProjectJobSchedulingConstraintProvider;
-import org.acme.projectjobschedule.rest.DemoDataGenerator;
+import org.acme.projectjobschedule.data.DemoDataGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,47 +27,7 @@ public class ProjectJobScheduleApp {
         SMALL,
         LARGE
     }
-    public static void ProjectJobSchedule(ProjectJobSchedule schedule) {
-        LOGGER.info("");
-        List<Project> projects = schedule.getProjects();
-        List<Job> jobs = schedule.getJobs();
 
-        Map<Project, List<Job>> projectJobMap = jobs.stream()
-                .filter(job -> job.getProject() != null)
-                .collect(Collectors.groupingBy(Job::getProject));
-
-        LOGGER.info("|  Project id    | Project | Job type   |");
-        LOGGER.info("|" + "-------------|------------|------------|");
-
-        for (Project project : projects) {
-            List<Job> projectJobs = projectJobMap.getOrDefault(project, Collections.emptyList());
-
-            if (projectJobs.isEmpty()) {
-                LOGGER.info("| " + String.format("%-11s", project.getReleaseDate()) + " | "
-                        + "No scheduled jobs".formatted() + " |");
-                continue;
-            }
-
-            for (Job job : projectJobs) {
-                LOGGER.info("| " + String.format("%-11s", project.getId()) + " | "
-                        + String.format("%-10s", job.getProject()) + " | "
-                        + String.format("%-10s", job.getJobType()) + " | ");
-            }
-            LOGGER.info("|" + "-------------|------------|------------|");
-        }
-
-        List<Job> unassignedJobs = jobs.stream()
-                .filter(job -> job.getProject() == null || job.getJobType() == null)
-                .toList();
-
-        if (!unassignedJobs.isEmpty()) {
-            LOGGER.info("");
-            LOGGER.info("Unassigned jobs:");
-            for (Job job : unassignedJobs) {
-                LOGGER.info("  " + job.getProject() + " - Job type: " + job.getJobType());
-            }
-        }
-    }
     public static void main(String[] args) {
         SolverFactory<ProjectJobSchedule> solverFactory = SolverFactory.create(new SolverConfig()
                 .withSolutionClass(ProjectJobSchedule.class)
@@ -86,10 +46,51 @@ public class ProjectJobScheduleApp {
         ProjectJobSchedule solution = solver.solve(problem);
 
         // Visualize the solution
-       //printProjectJobSchedule(solution);
+       printProjectJobSchedule(solution);
+    }
+    public static void  printProjectJobSchedule(ProjectJobSchedule schedule){
+            LOGGER.info("");
+            List<Project> projects = schedule.getProjects();
+            List<Job> jobs = schedule.getJobs();
+
+            Map<Project, List<Job>> projectJobMap = jobs.stream()
+                    .filter(job -> job.getProject() != null)
+                    .collect(Collectors.groupingBy(Job::getProject));
+
+            LOGGER.info("| Project id |                   Project                     | Job type   |");
+            LOGGER.info("|" + "------------|-----------------------------------------------|------------|");
+
+            for (Project project : projects) {
+                List<Job> projectJobs = projectJobMap.getOrDefault(project, Collections.emptyList());
+
+                if (projectJobs.isEmpty()) {
+                    LOGGER.info("| " + String.format("%-11s", project.getReleaseDate()) + " | "
+                            + "No scheduled jobs".formatted() + " |");
+                    continue;
+                }
+
+                for (Job job : projectJobs) {
+                    LOGGER.info("| " + String.format("%-10s", project.getId()) + " | "
+                            + String.format("%-11s", job.getProject()) + " | "
+                            + String.format("%-10s", job.getJobType()) + " | ");
+                }
+                LOGGER.info("|" + "------------|-----------------------------------------------|------------|");
+            }
+
+            List<Job> unassignedJobs = jobs.stream()
+                    .filter(job -> job.getProject() == null || job.getJobType() == null)
+                    .toList();
+
+            if (!unassignedJobs.isEmpty()) {
+                LOGGER.info("");
+                LOGGER.info("Unassigned jobs:");
+                for (Job job : unassignedJobs) {
+                    LOGGER.info("  " + job.getProject() + " - Job type: " + job.getJobType());
+                }
+            }
+        }
     }
 
-}
 
 
 
