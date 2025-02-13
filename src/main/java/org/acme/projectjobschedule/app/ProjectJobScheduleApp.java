f2286bd113899068343152fac8b626e4d7dd1a16
redCat <yana.sitkowecz@yandex.ru>
2025-02-13 12:14:29 +0300

    add condition one more time

--- src/main/java/org/acme/projectjobschedule/app/ProjectJobScheduleApp.java
+++ src/main/java/org/acme/projectjobschedule/app/ProjectJobScheduleApp.java
@@ -1,5 +1,6 @@
 package org.acme.projectjobschedule.app;
 
+import java.io.File;
 import java.util.List;
 
 import java.time.Duration;
@@ -18,43 +19,53 @@ import ai.timefold.solver.core.api.solver.Solver;
 
 import ai.timefold.solver.core.config.solver.SolverConfig;
 import ai.timefold.solver.core.config.solver.termination.TerminationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProjectJobScheduleApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectJobScheduleApp.class);
 
     public static void main(String[] args) {
 
         // Load the problem from JSON
         String filePath = "src/main/resources/importData.json"; // Путь к файлу JSON
-        
-        DataModel model = new DataModel(filePath);
-        model.readOperationHashMap();
-        ProjectJobSchedule problem = model.generateProjectJobSchedule();
-        SolverFactory<ProjectJobSchedule> solverFactory = SolverFactory.create(new SolverConfig()
-                .withSolutionClass(ProjectJobSchedule.class)
-                .withEntityClasses(Allocation.class)
-                .withConstraintProviderClass(ProjectJobSchedulingConstraintProvider.class)
-                // The solver runs only for 5 seconds on this small dataset.
-                // It's recommended to run for at least 5 minutes ("5m") otherwise.
-                .withTerminationConfig(new TerminationConfig()
-                        .withSpentLimit(Duration.ofSeconds(model.getTS())) // Максимум 5 минут
-                        .withUnimprovedSpentLimit(Duration.ofSeconds(model.getUS())))); // Или 1 минута без улучшений
 
-        // Solve the problem
-        Solver<ProjectJobSchedule> solver = solverFactory.buildSolver();
-        ProjectJobSchedule solution = solver.solve(problem);
+        File importFile = new File(filePath);
+        if(importFile.exists()) {
+            DataModel model = new DataModel(filePath);
+            model.readOperationHashMap();
+            ProjectJobSchedule problem = model.generateProjectJobSchedule();
+            SolverFactory<ProjectJobSchedule> solverFactory = SolverFactory.create(new SolverConfig()
+                    .withSolutionClass(ProjectJobSchedule.class)
+                    .withEntityClasses(Allocation.class)
+                    .withConstraintProviderClass(ProjectJobSchedulingConstraintProvider.class)
+                    // The solver runs only for 5 seconds on this small dataset.
+                    // It's recommended to run for at least 5 minutes ("5m") otherwise.
+                    .withTerminationConfig(new TerminationConfig()
+                            .withSpentLimit(Duration.ofSeconds(model.getTS())) // Максимум 5 минут
+                            .withUnimprovedSpentLimit(Duration.ofSeconds(model.getUS())))); // Или 1 минута без улучшений
 
-        SolutionManager<ProjectJobSchedule, HardMediumSoftScore> solutionManager = SolutionManager.create(solverFactory);
-        ScoreExplanation<ProjectJobSchedule, HardMediumSoftScore> scoreExplanation = solutionManager.explain(solution);
+            // Solve the problem
+            Solver<ProjectJobSchedule> solver = solverFactory.buildSolver();
+            ProjectJobSchedule solution = solver.solve(problem);
 
-        HardMediumSoftScore score = problem.getScore();
+            SolutionManager<ProjectJobSchedule, HardMediumSoftScore> solutionManager = SolutionManager.create(solverFactory);
+            ScoreExplanation<ProjectJobSchedule, HardMediumSoftScore> scoreExplanation = solutionManager.explain(solution);
 
-        List<Allocation> allocations = solution.getAllocations();
+            HardMediumSoftScore score = problem.getScore();
 
-        JsonExporter exporter = new JsonExporter(score, model.getID(),model.getStartDate(),
-                problem.getProjects(), problem.getResources(),problem.getResourceRequirements(),
-                allocations,scoreExplanation);
-        exporter.convertToJsonFile("src/main/resources/exportData.json");
+            List<Allocation> allocations = solution.getAllocations();
 
+            JsonExporter exporter = new JsonExporter(score, model.getID(), model.getStartDate(),
+                    problem.getProjects(), problem.getResources(), problem.getResourceRequirements(),
+                    allocations, scoreExplanation);
+            exporter.convertToJsonFile("src/main/resources/exportData.json");
+        }
+
+        else {
+            LOGGER.info("Import file not found in directory src/main/resources/ !");
+        }
     }
     }
 
